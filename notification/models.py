0c6f63bf44f4c165573a31865aed3d4adc31b975
TechByAbrar <engr.abrarfahim@gmail.com>
2026-02-01 14:29:54 +0600

    Notification models created for admin dashboard

--- notification/models.py
+++ notification/models.py
@@ -1,3 +1,29 @@
+# notifications/models.py
 from django.db import models
+from django.utils import timezone
+from typing import Final
 
-# Create your models here.
+class Notification(models.Model):
+    id = models.BigAutoField(primary_key=True)
+
+    type: Final[str] = "USER_CREATED"
+    event = models.CharField(max_length=50, db_index=True)
+
+    title = models.CharField(max_length=255)
+    message = models.TextField()
+
+    user_id = models.BigIntegerField(null=True, blank=True, db_index=True)  # who triggered
+    is_read = models.BooleanField(default=False, db_index=True)
+
+    created_at = models.DateTimeField(default=timezone.now, db_index=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [
+            models.Index(fields=["event"]),
+            models.Index(fields=["is_read"]),
+            models.Index(fields=["created_at"]),
+        ]
+
+    def __str__(self) -> str:
+        return self.title
